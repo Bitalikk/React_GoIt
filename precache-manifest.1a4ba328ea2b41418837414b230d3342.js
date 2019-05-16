2354a9e3dc04ad6791f6410c4a3feb89a16381e4
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-05-16 17:15:13 +0300

    Updates

---
+++ precache-manifest.1a4ba328ea2b41418837414b230d3342.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4c9cdf6bec819c05fdb90b868a5d7b",
+    "url": "/Rreact_GoIt/index.html"
+  },
+  {
+    "revision": "c4fd8b66e408f9f5148b",
+    "url": "/Rreact_GoIt/static/js/2.7f73a925.chunk.js"
+  },
+  {
+    "revision": "aeebe5d068427e27909c",
+    "url": "/Rreact_GoIt/static/js/main.dd910d21.chunk.js"
+  },
+  {
+    "revision": "784f6020d9c912a2eca9",
+    "url": "/Rreact_GoIt/static/js/runtime~main.1afb7d5b.js"
+  }
+]);
