294d9af50ed045f24b0a042f5134b87562e0cbae
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-05-18 10:00:23 +0300

    Homework01 added build

---
+++ build/precache-manifest.3fb5e7c47e9ef9c7ac2e1f81149a5915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774bbe6d93c9b179ce43cc00ceb71223",
+    "url": "/Rreact_GoIt/index.html"
+  },
+  {
+    "revision": "05cfd053d5cc8eb5304f",
+    "url": "/Rreact_GoIt/static/css/main.25695906.chunk.css"
+  },
+  {
+    "revision": "81fe4c3ba66bcbfe9a9b",
+    "url": "/Rreact_GoIt/static/js/2.269050a8.chunk.js"
+  },
+  {
+    "revision": "05cfd053d5cc8eb5304f",
+    "url": "/Rreact_GoIt/static/js/main.2ea75c44.chunk.js"
+  },
+  {
+    "revision": "784f6020d9c912a2eca9",
+    "url": "/Rreact_GoIt/static/js/runtime~main.1afb7d5b.js"
+  }
+]);
